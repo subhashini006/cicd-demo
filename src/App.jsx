b75298bfc7d78d2af669ab90b628305802618f92
subhashini006 <subhashini2ammu@gmail.com>
2026-09-24 10:37:07 +0530

    Fix build error

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ function App() {
   return (
     <div>
       <h1>Hello CI/CD</h1>
-      <p>This application was built automatically.</p>
+      <p>This application is built automatically.</p>
     </div>
-  )
+  );
 }
 
-export default App(
+export default App;
